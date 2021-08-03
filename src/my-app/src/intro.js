bd10a8af29294af794b63952838765a5b0e53586
alexking-byte <khkanazawa@gmail.com>
2021-08-03 01:38:19 +0000

    0803

---
+++ src/my-app/src/intro.js
@@ -0,0 +1,12 @@
+export default {
+    "00006peto": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '5ペットボトル'
+  },
+  "00007isu": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '7isu'
+  },
+}
---
+++ src/my-app/src/intro_backup.js
@@ -0,0 +1,62 @@
+export default {
+  "00001denshi": {
+    icon: '',
+    color:'#B43953',
+    info: '0乾電池'
+  },
+  "00002bentou": {
+    icon: '',
+    color:'#3F6BA8',
+    info: '1弁当箱'
+  },
+  "00003bin": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '2ビン'
+  },
+  "00004eakon": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '3エアコン'
+  },
+  "00005shiruba": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '4シルバーカート'
+  },
+  "00006peto": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '5ペットボトル'
+  },
+  "00007isu": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '6椅子'
+  },
+  "00008kan": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '7缶'
+  },
+  "00009tukue": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '8机'
+  },
+  "00010kunte": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '9軍手'
+  },
+  "00011reizoko": {
+    icon: '',
+    color:'#3F6BA7',
+    info: '10冷蔵庫'
+  },
+  "999999fumei": {
+    icon: '',
+    color:'#48925B',
+    info: '99不明'
+  },
+}
